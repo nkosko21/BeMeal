d020acfcec6ec1e96b01ccbd4af52b407db3f1a7
nkosko21 <nickoy7@gmail.com>
2023-06-09 19:36:24 -0400

    Initial Commit

---
+++ BeMeal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeMeal
+//
+//  Created by Nick Kosko on 6/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
